976002290f5d040364875c942e8eff54da519c7c
Malcome Baggett <MalcomeBaggett@gmail.com>
2023-11-21 11:27:03 -0500

    added benchmarking to iteration tests

--- iteration/iteration_test.go
+++ iteration/iteration_test.go
@@ -5,10 +5,16 @@ import (
 )
 
 func TestRepeat(t *testing.T) {
-	repeated := Repeat("a")
+	repeated := Repeat("a", 5)
 	expected := "aaaaa"
 
 	if repeated != expected {
 		t.Errorf("expected %q but got %q", expected, repeated)
+	}
+}
+
+func BenchmarkReapeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a", 5)
 	}
 }
